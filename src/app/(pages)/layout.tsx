c29b6dba6211b106b212c1fb0efacaa798cb9560
shrage <shreyansh3536@gmail.com>
2025-12-30 12:46:23 +0545

    added scripts

--- src/app/(pages)/layout.tsx
+++ src/app/(pages)/layout.tsx
@@ -33,6 +33,28 @@ export default function RootLayout({ children }: RootLayoutProps) {
           src="https://checkout.razorpay.com/v1/checkout.js"
           strategy="afterInteractive"
         />
+        <Script async src="https://www.googletagmanager.com/gtag/js?id=G-8ZL8763359" />
+        <Script id="ga-init" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || []
+            function gtag() {
+                dataLayer.push(arguments)
+            }
+            gtag('js', new Date());
+            gtag('config', 'G-8ZL8763359')
+          `}
+        </Script>
+        <Script id="clairt-init" type="text/javascript">
+            {`
+            (function(c,l,a,r,i,t,y)  {
+                c[a] = c[a] || function () { (c[a].q = c[a].q || []).push(arguments) };
+                t=l.createElement(r);
+                t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                y=l.getElementsByTagName(r)[0];
+                y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "uthr0z0hl7");
+      `}
+        </Script>
       </head>
       <body className={dmSans.variable}>
         <Providers>
