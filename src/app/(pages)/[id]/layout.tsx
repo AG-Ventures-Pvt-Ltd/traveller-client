e3f7203fac706ae67ada77a45dab8cadad070f7f
shrage <shreyansh3536@gmail.com>
2026-06-23 23:46:35 +0530

    bio fix

--- src/app/(pages)/[id]/layout.tsx
+++ src/app/(pages)/[id]/layout.tsx
@@ -58,7 +58,7 @@ export async function generateMetadata({
             siteName: 'Wondrr',
             url,
             title,
-            description: generatedDescription,
+            description: meta?.bio || generatedDescription,
             ...(avatar && {
                 images: [{ url: avatar, alt: fullName, width: 1200, height: 630 }],
             }),
